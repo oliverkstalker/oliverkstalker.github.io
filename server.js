205f2a2eeb11399506f1e619d05dfb7be564a31f
oliverkstalker <oliver.stalker@outlook.com>
2025-04-05 21:48:22 -0500

    incorrect path name fix

--- server.js
+++ server.js
@@ -34,7 +34,11 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Serve static files from the public folder
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Sample data (for API purposes if needed)
 let animations = [
